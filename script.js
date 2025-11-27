33be72a4a9e9bef0523e162947cf6e63fe5d6460
Liyutsa Zirange <Liyutsa@gmail.com>
2025-11-27 18:02:12 +0000

    Finished

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-document.getElementById("msg").addEventListener("click", function () {
-  window.open("mailto:Liyutsa@gmail.com")
-})
-
 document.getElementById("Contact").addEventListener("click", function () {
   window.open("contact.html")
 })
 
-document.getElementById("work").addEventListener("click", function () {
-  window.open("work.html")
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const scrollElements = document.querySelectorAll(".animate-on-scroll");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show"); // in view → show
+        } else {
+          entry.target.classList.remove("show"); // out of view → hide
+        }
+      });
+    },
+    { threshold: 0.2 } // trigger when 20% of element is visible
+  );
+
+  scrollElements.forEach(el => observer.observe(el));
+});
+
