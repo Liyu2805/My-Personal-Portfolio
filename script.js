b15f1b944f0ec94dae50fa9614b36cb2f3d1d6c2
Liyutsa Zirange <Liyutsa@gmail.com>
2025-11-27 13:35:24 +0000

    got started

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("msg").addEventListener("click", function () {
+  window.open("mailto:Liyutsa@gmail.com")
+})
+
+document.getElementById("Contact").addEventListener("click", function () {
+  window.open("contact.html")
+})
+
+document.getElementById("work").addEventListener("click", function () {
+  window.open("work.html")
+})
